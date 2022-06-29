461774246330dbbbb414df1b4c8e5356f6f8fcf6
Adam Murdoch <adam@gradle.com>
2022-06-30 08:47:41 +1000

    Fix calculation of the node dependencies of an artifact transform that takes upstream dependencies as input, when those upstream dependencies include an external dependency that conflicts with a project.
    The basic problem is that the full set of node dependencies for a transform are not known until the entire artifact dependency graph has been resolved and all conflicts resolved. However, this does not always happen at work graph calculation time and some node dependencies are "discovered" at execution time.
    
    In this commit, use the pre- and post-execution node concepts to allow the "finalize upstream dependencies for transform" node to delay the transform until all node dependencies of the transform have been discovered and completed.

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultTransformUpstreamDependenciesResolver.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultTransformUpstreamDependenciesResolver.java
@@ -16,6 +16,8 @@
 
 package org.gradle.api.internal.artifacts.transform;
 
+import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.artifacts.component.ComponentIdentifier;
 import org.gradle.api.artifacts.result.DependencyResult;
 import org.gradle.api.artifacts.result.ResolutionResult;
@@ -29,15 +31,22 @@ import org.gradle.api.internal.file.FileCollectionInternal;
 import org.gradle.api.internal.project.ProjectInternal;
 import org.gradle.api.internal.tasks.NodeExecutionContext;
 import org.gradle.api.internal.tasks.TaskDependencyResolveContext;
+import org.gradle.api.internal.tasks.WorkNodeAction;
 import org.gradle.api.specs.Spec;
+import org.gradle.execution.plan.PostExecutionNodeAwareActionNode;
+import org.gradle.execution.plan.TaskNode;
+import org.gradle.execution.plan.TaskNodeFactory;
 import org.gradle.internal.Describables;
 import org.gradle.internal.Try;
 import org.gradle.internal.model.CalculatedValueContainer;
 import org.gradle.internal.model.CalculatedValueContainerFactory;
 import org.gradle.internal.model.ValueCalculator;
 
+import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -179,7 +188,7 @@ public class DefaultTransformUpstreamDependenciesResolver implements TransformUp
         }
 
         @Override
-        public FileCollection selectedArtifacts() {
+        public FileCollectionInternal selectedArtifacts() {
             return selectedArtifactsFor(fromAttributes);
         }
 
@@ -191,6 +200,53 @@ public class DefaultTransformUpstreamDependenciesResolver implements TransformUp
         @Override
         public ProjectInternal getOwningProject() {
             return owner.getProject();
+        }
+
+        @Nullable
+        @Override
+        public WorkNodeAction getPreExecutionAction() {
+            return new PostExecutionNodeAwareActionNode() {
+                final List<TaskNode> tasks = new ArrayList<>();
+
+                @Override
+                public boolean usesMutableProjectState() {
+                    return FinalizeTransformDependenciesFromSelectedArtifacts.this.usesMutableProjectState();
+                }
+
+                @Nullable
+                @Override
+                public Project getOwningProject() {
+                    return FinalizeTransformDependenciesFromSelectedArtifacts.this.getOwningProject();
+                }
+
+                @Override
+                public void run(NodeExecutionContext context) {
+                    TaskNodeFactory taskNodeFactory = context.getService(TaskNodeFactory.class);
+                    selectedArtifacts().visitDependencies(new TaskDependencyResolveContext() {
+                        @Override
+                        public void add(Object dependency) {
+                            if (dependency instanceof Task) {
+                                tasks.add(taskNodeFactory.getNode((Task) dependency));
+                            }
+                        }
+
+                        @Override
+                        public void visitFailure(Throwable failure) {
+                        }
+
+                        @Nullable
+                        @Override
+                        public Task getTask() {
+                            return null;
+                        }
+                    });
+                }
+
+                @Override
+                public List<TaskNode> getPostExecutionNodes() {
+                    return tasks;
+                }
+            };
         }
 
         @Override
